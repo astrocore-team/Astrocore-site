ad2a8895c3d2e774c1151dc56d4fa19b07d69323
JSman225 <kmleonardi@kmail.org>
2022-07-09 17:37:46 -0400

    this is a test for github+discord

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 window.addEventListener("scroll", function() {
     let pageHeight = window.innerHeight;
+    if (window.scrollY > pageHeight*.1) {
+        this.document.getElementById('what-we-do').classList.add("fadeInBottom");
+        this.document.getElementById('what-we-do').classList.add("cssanimation");
+    }
+  });
+
+
+
+
+
+
+window.addEventListener("scroll", function() {
+    let pageHeight = window.innerHeight;
     if (window.scrollY > (pageHeight*1.2)-10) {
         this.document.getElementById('testamonies').classList.add("change");
     }
