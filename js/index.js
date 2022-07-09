a4b9517520dfde3dfe96b8ce01d8917f6561e7a8
JSman225 <kmleonardi@kmail.org>
2022-07-09 18:39:57 -0400

    got the shapes spinning

--- js/index.js
+++ js/index.js
@@ -6,9 +6,32 @@ window.addEventListener("scroll", function() {
     }
   });
 
-
-
-
+  let count = 1;
+  let oldValue = 0
+let newValue = 0
+  window.addEventListener("scroll", function() {
+    newValue = window.pageYOffset;
+    if (oldValue < newValue) {
+      this.document.getElementById('spin1').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin2').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin3').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin4').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin5').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin6').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin7').style.transform = 'rotate('+count+'deg)';
+      count = count+3;
+    } else if (oldValue > newValue) {
+      this.document.getElementById('spin1').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin2').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin3').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin4').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin5').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin6').style.transform = 'rotate('+count+'deg)';
+      this.document.getElementById('spin7').style.transform = 'rotate('+count+'deg)';
+      count = count-3;
+    }
+    oldValue = newValue;
+  });
 
 
 window.addEventListener("scroll", function() {
