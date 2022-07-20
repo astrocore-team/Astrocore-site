d4221bd542ed5171a917c611ba7f919f197ffcc4
JSman225 <kmleonardi@kmail.org>
2022-07-20 16:17:44 -0400

    I did a lot of stuff man i duno. if you are so curious read the changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,46 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
 window.addEventListener("scroll", function() {
     let pageHeight = window.innerHeight;
     if (window.scrollY > pageHeight*.1) {
@@ -40,13 +83,75 @@ window.addEventListener("scroll", function() {
         this.document.getElementById('staff11').classList.add("fadeInBottom");
         this.document.getElementById('staff12').classList.add("cssanimation");
         this.document.getElementById('staff12').classList.add("fadeInBottom");
+    }
+  });
+
+
+function bioClose(){
+  this.document.getElementById('bio-wrapper').classList.remove("bio-wrapper-active");
+  this.document.getElementById('bio-card').classList.remove("bio-card-active"); 
+  this.document.body.style.overflowY = 'scroll';
+  this.document.body.style.overflowX = 'hidden';
+  enableScroll();
+}
+
+fetch("json/test.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+
+  window.addEventListener('click', function(e){
+    if(document.getElementById('staff1').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff2').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff3').contains(e.target)){
+      disableScroll()
+      let pageHeight = window.innerHeight; window.scrollTo({ top:pageHeight*1.9, behavior: 'smooth' });
+      this.document.getElementById('bio-wrapper').classList.add("bio-wrapper-active");
+      this.document.getElementById('bio-card').classList.add("bio-card-active"); 
+      this.document.body.style.overflow = 'hidden';
+      // ------------- ENTER INFO HERE  -------------
+      this.document.getElementById('bio-image').src="assets/JSman225.png";
+      this.document.getElementById('bio-name').innerHTML = "JSman225";
+      this.document.getElementById('bio-text').innerHTML = "This is a bio about the person. They can put a short about themselves paragraph here. It really should not be too long, but still long enough because I have given you all plent of space.";
+      this.document.getElementById('bio-pos1').innerHTML = "- Senior Web Developer";
+      this.document.getElementById('bio-pos2').innerHTML = "- Senior Web Designer";
+      this.document.getElementById('bio-pos3').innerHTML = "- Senior Python Developer";
+      this.document.getElementById('bio-pos4').innerHTML = "";
+    }
+    else if(document.getElementById('staff4').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff5').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff6').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff7').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff8').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff9').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff10').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff11').contains(e.target)){
+
+    }
+    else if(document.getElementById('staff12').contains(e.target)){
 
     }
   });
 
-  function bio(number){
-    var circle = document.getElementsByClassName("staff-image-wrapper")[number];
-  }
+
 
 
 
